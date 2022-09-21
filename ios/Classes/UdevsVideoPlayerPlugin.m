5b02b1ed5c73d88fc853721b22c283825a089f65
Sunnatillo Shavkatov <sunnatillo.shavkatov@udevs.io>
2022-09-21 18:21:02 +0500

    ios done

--- ios/Classes/UdevsVideoPlayerPlugin.m
+++ ios/Classes/UdevsVideoPlayerPlugin.m
@@ -1,15 +1,8 @@
 #import "UdevsVideoPlayerPlugin.h"
-#if __has_include(<udevs_video_player/udevs_video_player-Swift.h>)
 #import <udevs_video_player/udevs_video_player-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "udevs_video_player-Swift.h"
-#endif
 
 @implementation UdevsVideoPlayerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [UdevsVideoPlayerPlugin registerWithRegistrar:registrar];
+  [SwiftUdevsVideoPlayerPlugin registerWithRegistrar:registrar];
 }
 @end
