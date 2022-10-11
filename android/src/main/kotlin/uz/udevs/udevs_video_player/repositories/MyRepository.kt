661ebd1d94bf5ee5484eae049653853586c8de02
Sunnatillo Shavkatov <sunnatillo.shavkatov@udevs.io>
2022-10-11 14:37:51 +0500

    video player bug fix

--- android/src/main/kotlin/uz/udevs/udevs_video_player/repositories/MyRepository.kt
+++ android/src/main/kotlin/uz/udevs/udevs_video_player/repositories/MyRepository.kt
@@ -1,60 +1,62 @@
-package uz.udevs.udevs_video_player.repositories
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import uz.udevs.udevs_video_player.models.MegogoStreamResponse
-import uz.udevs.udevs_video_player.models.PremierStreamResponse
-import uz.udevs.udevs_video_player.retrofit.Common
-import uz.udevs.udevs_video_player.retrofit.RetrofitService
-
-class MyRepository {
-
-    private var retrofitService: RetrofitService = Common.retrofitService()
-
-    fun getMegogoStream(
-        sessionId: String,
-        videoId: String,
-        accessToken: String,
-    ): LiveData<MegogoStreamResponse> {
-        val data = MutableLiveData<MegogoStreamResponse>()
-        retrofitService.getMegogoStream(sessionId, videoId, accessToken)
-            .enqueue(object : Callback<MegogoStreamResponse> {
-                override fun onResponse(
-                    call: Call<MegogoStreamResponse>,
-                    response: Response<MegogoStreamResponse>
-                ) {
-                    data.value = response.body()
-                }
-
-                override fun onFailure(call: Call<MegogoStreamResponse>, t: Throwable) {
-                    data.value = null
-                }
-            })
-        return data
-    }
-
-    fun getPremierStream(
-        sessionId: String,
-        videoId: String,
-        episodeId: String,
-    ): LiveData<PremierStreamResponse> {
-        val data = MutableLiveData<PremierStreamResponse>()
-        retrofitService.getPremierStream(sessionId, videoId, episodeId)
-            .enqueue(object : Callback<PremierStreamResponse> {
-                override fun onResponse(
-                    call: Call<PremierStreamResponse>,
-                    response: Response<PremierStreamResponse>
-                ) {
-                    data.value = response.body()
-                }
-
-                override fun onFailure(call: Call<PremierStreamResponse>, t: Throwable) {
-                    data.value = null
-                }
-            })
-        return data
-    }
-}
+//package uz.udevs.udevs_video_player.repositories
+//
+//import androidx.lifecycle.LiveData
+//import androidx.lifecycle.MutableLiveData
+//import retrofit2.Call
+//import retrofit2.Callback
+//import retrofit2.Response
+//import uz.udevs.udevs_video_player.models.MegogoStreamResponse
+//import uz.udevs.udevs_video_player.models.PremierStreamResponse
+//import uz.udevs.udevs_video_player.retrofit.Common
+//import uz.udevs.udevs_video_player.retrofit.RetrofitService
+//
+//class MyRepository {
+//
+//    private var retrofitService: RetrofitService = Common.retrofitService()
+//
+//    fun getMegogoStream(
+//        authorization: String,
+//        sessionId: String,
+//        videoId: String,
+//        accessToken: String,
+//    ): LiveData<MegogoStreamResponse> {
+//        val data = MutableLiveData<MegogoStreamResponse>()
+//        retrofitService.getMegogoStream(authorization,sessionId, videoId, accessToken)
+//            .enqueue(object : Callback<MegogoStreamResponse> {
+//                override fun onResponse(
+//                    call: Call<MegogoStreamResponse>,
+//                    response: Response<MegogoStreamResponse>
+//                ) {
+//                    data.value = response.body()
+//                }
+//
+//                override fun onFailure(call: Call<MegogoStreamResponse>, t: Throwable) {
+//                    data.value = null
+//                }
+//            })
+//        return data
+//    }
+//
+//    fun getPremierStream(
+//        authorization: String,
+//        sessionId: String,
+//        videoId: String,
+//        episodeId: String,
+//    ): LiveData<PremierStreamResponse> {
+//        val data = MutableLiveData<PremierStreamResponse>()
+//        retrofitService.getPremierStream(authorization, sessionId, videoId, episodeId)
+//            .enqueue(object : Callback<PremierStreamResponse> {
+//                override fun onResponse(
+//                    call: Call<PremierStreamResponse>,
+//                    response: Response<PremierStreamResponse>
+//                ) {
+//                    data.value = response.body()
+//                }
+//
+//                override fun onFailure(call: Call<PremierStreamResponse>, t: Throwable) {
+//                    data.value = null
+//                }
+//            })
+//        return data
+//    }
+//}
