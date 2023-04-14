e25452f26dd7259ae14894d3a6cc6e3d81322e01
bekzod <bbkxaladin@gmail.com>
2023-04-14 11:15:07 +0500

    mobile resize bug fix

--- lib/udevs_video_player.dart
+++ lib/udevs_video_player.dart
@@ -23,49 +23,49 @@ class UdevsVideoPlayer {
 
   Future<int?> playVideo({required PlayerConfiguration playerConfig}) {
 
-    // String jsonStringConfig = jsonEncode(playerConfig.toJson());
-    String jsonStringConfig = jsonEncode({
-      "initialResolution": {
-        "Auto":
-            "https://st1.uzdigital.tv/Animal_Planet/video.m3u8?token=20c4ea3df0ba43ac0feebbe1bb3fdff82d2971ed-78717a74766e586e6f50644f69724747-1681455918-1681445118&remote=94.232.24.122"
-      },
-      "resolutions": {
-        "Auto":
-            "https://st1.uzdigital.tv/Animal_Planet/video.m3u8?token=20c4ea3df0ba43ac0feebbe1bb3fdff82d2971ed-78717a74766e586e6f50644f69724747-1681455918-1681445118&remote=94.232.24.122",
-        "480p":
-            "http://st1.uzdigital.tv/Animal_Planet/tracks-v1a1a2/mono.m3u8?remote=94.232.24.122&token=20c4ea3df0ba43ac0feebbe1bb3fdff82d2971ed-78717a74766e586e6f50644f69724747-1681455918-1681445118&remote=94.232.24.122"
-      },
-      "qualityText": "Качество",
-      "speedText": "Скорость",
-      "lastPosition": 0,
-      "title": "Animal Planet",
-      "isSerial": false,
-      "seasons": [],
-      "isLive": true,
-      "tvProgramsText": "Телепередачи",
-      "programsInfoList": [
-        {"day": "Yesterday", "tvPrograms": []},
-        {"day": "Today", "tvPrograms": []},
-        {"day": "Tomorrow", "tvPrograms": []}
-      ],
-      "showController": true,
-      "playVideoFromAsset": false,
-      "assetPath": "",
-      "seasonIndex": 0,
-      "episodeIndex": 0,
-      "episodeText": "эпизод",
-      "seasonText": "Сезон",
-      "isMegogo": false,
-      "isPremier": false,
-      "videoId": "",
-      "sessionId": "64368bc00ef5171d45e9a8b4",
-      "megogoAccessToken":
-          "MToxMjc3MzAzMTc3OjE2ODEzMDQ3NzQ6OjYyMWYxMDYzODA5MjZkZWQ3Y2JkOGIzNzJkMzg5NWZm",
-      "authorization":
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODEyOTYzMjAsImlzcyI6InVzZXIiLCJwaWQiOjQ0NjEyLCJyb2xlIjoiY3VzdG9tZXIiLCJzdWIiOiI3NTA1MjdhMS0yMjdjLTRhYTUtYjY2MC04NjQzNjA5NzA0OTUifQ.D9wCndt8JCXvK5-HkSxCSikOouWra6hydussIJ9xclQ",
-      "autoText": "Автонастройка",
-      "baseUrl": "https://api.spec.uzd.udevs.io/v1/"
-    });
+    String jsonStringConfig = jsonEncode(playerConfig.toJson());
+    // String jsonStringConfig = jsonEncode({
+    //   "initialResolution": {
+    //     "Auto":
+    //         "https://st1.uzdigital.tv/Animal_Planet/video.m3u8?token=20c4ea3df0ba43ac0feebbe1bb3fdff82d2971ed-78717a74766e586e6f50644f69724747-1681455918-1681445118&remote=94.232.24.122"
+    //   },
+    //   "resolutions": {
+    //     "Auto":
+    //         "https://st1.uzdigital.tv/Animal_Planet/video.m3u8?token=20c4ea3df0ba43ac0feebbe1bb3fdff82d2971ed-78717a74766e586e6f50644f69724747-1681455918-1681445118&remote=94.232.24.122",
+    //     "480p":
+    //         "http://st1.uzdigital.tv/Animal_Planet/tracks-v1a1a2/mono.m3u8?remote=94.232.24.122&token=20c4ea3df0ba43ac0feebbe1bb3fdff82d2971ed-78717a74766e586e6f50644f69724747-1681455918-1681445118&remote=94.232.24.122"
+    //   },
+    //   "qualityText": "Качество",
+    //   "speedText": "Скорость",
+    //   "lastPosition": 0,
+    //   "title": "Animal Planet",
+    //   "isSerial": false,
+    //   "seasons": [],
+    //   "isLive": true,
+    //   "tvProgramsText": "Телепередачи",
+    //   "programsInfoList": [
+    //     {"day": "Yesterday", "tvPrograms": []},
+    //     {"day": "Today", "tvPrograms": []},
+    //     {"day": "Tomorrow", "tvPrograms": []}
+    //   ],
+    //   "showController": true,
+    //   "playVideoFromAsset": false,
+    //   "assetPath": "",
+    //   "seasonIndex": 0,
+    //   "episodeIndex": 0,
+    //   "episodeText": "эпизод",
+    //   "seasonText": "Сезон",
+    //   "isMegogo": false,
+    //   "isPremier": false,
+    //   "videoId": "",
+    //   "sessionId": "64368bc00ef5171d45e9a8b4",
+    //   "megogoAccessToken":
+    //       "MToxMjc3MzAzMTc3OjE2ODEzMDQ3NzQ6OjYyMWYxMDYzODA5MjZkZWQ3Y2JkOGIzNzJkMzg5NWZm",
+    //   "authorization":
+    //       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODEyOTYzMjAsImlzcyI6InVzZXIiLCJwaWQiOjQ0NjEyLCJyb2xlIjoiY3VzdG9tZXIiLCJzdWIiOiI3NTA1MjdhMS0yMjdjLTRhYTUtYjY2MC04NjQzNjA5NzA0OTUifQ.D9wCndt8JCXvK5-HkSxCSikOouWra6hydussIJ9xclQ",
+    //   "autoText": "Автонастройка",
+    //   "baseUrl": "https://api.spec.uzd.udevs.io/v1/"
+    // });
     return UdevsVideoPlayerPlatform.instance.playVideo(
       playerConfigJsonString: jsonStringConfig,
     );
