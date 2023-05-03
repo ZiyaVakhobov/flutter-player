92b54ea40389bfa055acaf1dcff2b38c995ea99f
Sunnatillo Shavkatov <sunnatilloshavkatov@gmail.com>
2023-05-03 17:27:29 +0500

    change orientation done

--- ios/Classes/Player/VideoPlayer/PlayerView.swift
+++ ios/Classes/Player/VideoPlayer/PlayerView.swift
@@ -440,7 +440,6 @@ class PlayerView: UIView {
     }
     
     @objc func exitButtonPressed(_ sender: UIButton){
-        delegate?.changeOrientation()
         purgeMediaPlayer();
         removeMediaPlayerObservers();
         delegate?.close(duration: player.currentTime().seconds)
