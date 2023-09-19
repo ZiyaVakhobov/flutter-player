c3b6592bb9c4c218e7a117d9381a71f65ccca52b
Sunnatillo <sunnatilloshavkatov@gmail.com>
2023-09-19 10:48:32 +0500

    tv channel model ui done

--- ios/Classes/Player/VideoPlayer/VideoPlayerViewController.swift
+++ ios/Classes/Player/VideoPlayer/VideoPlayerViewController.swift
@@ -426,7 +426,7 @@ class VideoPlayerViewController: UIViewController, AVPictureInPictureControllerD
         let episodeVC = CollectionViewController()
         episodeVC.modalPresentationStyle = .custom
         episodeVC.channels = self.playerConfiguration.channels
-        episodeVC.tv = ["Все каналы", "Кино и сериалы","Познавательные", "Новостные", "Музыкальные","Федеральные" ]
+        episodeVC.tv = self.playerConfiguration.tvCategories
         episodeVC.delegate = self
         self.present(episodeVC, animated: true, completion: nil)
     }
